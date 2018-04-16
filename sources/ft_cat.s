7e53ed6fd540250f2938c8dc69b8d81c4e04460d
Jonathan Carra <jcarra@student.42.fr>
2018-04-16 21:29:04 +0200

    Doc ft_cat.s

--- sources/ft_cat.s
+++ sources/ft_cat.s
@@ -5,50 +5,50 @@
 %define BUFFER_SIZE		1024
 
 section .bss
-	buffer resb BUFFER_SIZE+1
+	buffer resb BUFFER_SIZE+1	; Tableau de char de taille BUFFER_SIZE+1
 
 section .text
 	global _ft_cat
 	extern _ft_memset
-	
+
 _ft_cat:
-	push r8
-	push r9
-	
-	mov r8, rdi
-	jmp _ft_cat_read
+	push r8				; Sauve r8 sur la pile
+	push r9				; Sauve r9 sur la pile
+
+	mov r8, rdi			; Set r8 a fd
+	jmp _ft_cat_read		; Va a _ft_cat_read
 
 _ft_cat_read:
-	lea rdi, [rel buffer]
-	mov rsi, 0
-	mov rdx, BUFFER_SIZE+1
-	call _ft_memset
+	lea rdi, [rel buffer]		; Param 1 = buffer
+	mov rsi, 0			; Param 2 = '\0'
+	mov rdx, BUFFER_SIZE+1		; Param 3 = BUFFER_SIZE + 1
+	call _ft_memset			; Call ft_memset()
 
-	mov rdi, r8
-	lea rsi, [rel buffer]
-	mov rdx, BUFFER_SIZE
-	mov rax, MACH_SYSCALL(READ)
-	syscall
+	mov rdi, r8			; Param 1 = fd
+	lea rsi, [rel buffer]		; Param 2 = buffer
+	mov rdx, BUFFER_SIZE		; Param 3 = BUFFER_SIZE
+	mov rax, MACH_SYSCALL(READ)	; Init read
+	syscall				; Call read()
 
-	mov r9, rax
+	mov r9, rax			; Set r9 au retour de read
 	cmp r9, 0
-	jle _ft_cat_end
-	jmp _ft_cat_write
+	jle _ft_cat_end			; Si le retour de read est <= a 0 va a _ft_cat_end
+	jmp _ft_cat_write		; Va a _ft_cat_write
 
 _ft_cat_write:
-	lea rsi, [rel buffer]
-	mov rdi, STDOUT
-	mov rdx, r9
-	mov rax, MACH_SYSCALL(WRITE)
-	syscall
+	lea rsi, [rel buffer]		; Param 1 = buffer
+	mov rdi, STDOUT			; Param 2 = STDOUT
+	mov rdx, r9			; Param 3 = retour de read
+	mov rax, MACH_SYSCALL(WRITE)	; Init write
+	syscall				; Call write()
 
 	cmp r9, BUFFER_SIZE
-	jl _ft_cat_end
-	
-	jmp _ft_cat_read
+	jl _ft_cat_end			; Si le retour de read est < a BUFFER_SIZE va a _ft_cat_end
+
+	jmp _ft_cat_read		; Va a _ft_cat_read
 
 _ft_cat_end:
-	pop r9
-	pop r8
-	
-	ret
+	pop r9				; Charge r9 depuis la pile
+	pop r8				; Charge r8 depuis la pile
+
+	ret				; return(void);
